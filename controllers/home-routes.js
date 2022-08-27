c269f56261dec8b4acfb539d04a50423a65a467e
bpiper91 <bpiper91@gmail.com>
2022-08-27 16:37:02 -0400

    added sessions using express, added login/logout logic and buttons

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
+// retrieve all posts
 router.get('/', (req, res) => {
+    console.log(req.session);
+
     Post.findAll({
         attributes: [
             'id',
@@ -34,6 +37,16 @@ router.get('/', (req, res) => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+// sign up or log in
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    };
+    
+    res.render('login');
 });
 
 module.exports = router;
